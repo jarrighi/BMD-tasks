aafb7801e251ca5635325b0b101b261040469ef5
Juliana Arrighi <juliana.arrighi@gmail.com>
2016-12-21 20:43:11 -0800

    Command takes one or more word arguments

--- booksearch/booksearch.py
+++ booksearch/booksearch.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 
 DEFAULT_FILE = 'bookdata.json'
-bookfile = DEFAULT_FILE
 
 class BookSearchParser(argparse.ArgumentParser):
   def error(self, message):
@@ -12,10 +11,8 @@ class BookSearchParser(argparse.ArgumentParser):
 
 def create_parser():
   parser = BookSearchParser()
-  # Add arguments here
   parser.add_argument('-f', '--file', nargs=1, help="JSON File to be searched.")
-
-
+  parser.add_argument('words', nargs='+')
   return parser
 
 def create_index(file):
@@ -24,6 +21,15 @@ def create_index(file):
 def search(word, index):
   pass
 
+def print_output(bookfile):
+  print('Using "{}" as book data source.'.format(bookfile))
+
+def set_data_file(args):
+  bookfile = DEFAULT_FILE
+  if args.file:
+    bookfile = args.file[0]
+  return bookfile
+
 def main():
   # Create parser
   parser = create_parser()
@@ -31,11 +37,14 @@ def main():
   # Parse Args
   args = parser.parse_args()
 
+  bookfile = set_data_file(args)
+
   # Create Index
 
   # Run Searches
 
   # Print Results
+  print_output(bookfile)
 
 if __name__ == '__main__':
   main()
--- booksearch/tests.py
+++ booksearch/tests.py
@@ -6,33 +6,31 @@ import sys
 class RunCommandTest(unittest.TestCase):
 
   def setUp(self):
-    pass
+    self.parser = create_parser()
 
   def tearDown(self):
     pass
 
   def test_returns_help_with_help_arg(self):
-    parser = create_parser()
     with self.assertRaises(SystemExit) as cm:
-      parser.parse_args(['-h'])
-      
+      self.parser.parse_args(['-h'])
     output = sys.stdout.getvalue().strip()
     expected = "usage: tests.py [-h]"
     self.assertIn(expected, output)
 
-  def test_command_takes_any_number_of_word_args(self):
-    pass
+  def test_command_takes_one_or_more_of_word_args(self):
+    args = self.parser.parse_args(['hello', 'world'])
+    
 
   def test_takes_optional_data_file(self):
-    parser = create_parser()
-    args = parser.parse_args(['-f', 'bookdata_small.json'])
+    args = self.parser.parse_args(['-f', 'bookdata_small.json', 'word'])
     self.assertEqual(args.file[0], 'bookdata_small.json')
+    bookfile = set_data_file(args)
     self.assertEqual(bookfile, 'bookdata_small.json')
 
   def test_returns_help_with_no_args(self):
-    parser = create_parser()
     with self.assertRaises(SystemExit) as x:
-      parser.parse_args()
+      self.parser.parse_args()
 
     output = sys.stdout.getvalue().strip()
     expected = "usage: tests.py [-h]"
@@ -40,9 +38,8 @@ class RunCommandTest(unittest.TestCase):
 
 
   def test_returns_help_with_invalid_args(self):
-    parser = create_parser()
     with self.assertRaises(SystemExit) as x:
-      parser.parse_args(["-u"])
+      self.parser.parse_args(["-u"])
 
     output = sys.stdout.getvalue().strip()
     expected = "usage: tests.py [-h]"
